d60fa036e3f40bb3dfcdb32f60ddd1002799b124
JJ <jjjay@mac.com>
2018-04-15 14:38:26 +0100

    add in insecure notice for github-gist

--- src/blocks/github-gist/block.php
+++ src/blocks/github-gist/block.php
@@ -16,6 +16,8 @@ add_action( 'init', 'mjj_why_github_gist_init' );
 
 function mjj_why_github_gist_render( $attributes ){
 
+	// if you use this as a check and put it on your site without a csp, let me know and I will hack you :)
+	// no but seriously, this is going to be insecure, you will need to do better checks
 	if ( empty( $attributes['gistUrl'] )
 		|| 'gist.github.com' !== parse_url( $attributes['gistUrl'], PHP_URL_HOST ) ) {
 			return '';
